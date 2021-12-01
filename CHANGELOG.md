ca2814391eb7a0c6db700fc0c8a56511fc43d050
lmgveerhoek <lmg@veerhoek.eu>
2021-12-01 16:19:50 +0100

    1-4 done

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,23 @@
 ## 0.1.0.0 -- YYYY-mm-dd
 
 * First version. Released on an unsuspecting world.
+
+
+
+## Notes from teams: 
+Testing functions can be done through: https://putridparrot.com/blog/the-haskell-repl-basic-commands/
+
+- Using a library to deal with time / leap years is totally fine
+- Using a library for prettyprinting the calendar is highly encouraged
+- Using a library (other than uu-tc) for parsing is generally not allowed 
+
+- Are we allowed to move the instance Show of DateTime from Main.hs to DateTime.hs?
+By having the instance Show inside DateTime, it is reusable in Calendar.hs when deriving Show on other datatypes. Main.hs can't be imported here because of the cyclic dependency.
+
+- Should we take leap years into account? 
+Note that there is also a Time library that has a function that does this for you
+
+- Hi, I have a question about exercise 11. And in particular bullet point 4: "How much time (in minutes) is spent in total for events with a given summary?"  Is it possible that these events are longer than a few hours? For example a few days or even a month? Because this will make the logic a lot more difficult 
+
+Good question: It is not a central part of this assignment, so if you do all your time calculations yourself I think it is completely fine to add a comment "this function assumes that events stay within one day" for example.
+If you use a library for dealing with time calculations (which is allowed!), then I hope that that library has a simple 'time difference' function 
--- src/DateTime.hs
+++ src/DateTime.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE RecordWildCards #-}
 module DateTime where
 
 import ParseLib.Abstract
 import Prelude hiding ((<$), ($>), (<*), (*>), sequence)
+import Data.Tuple
+import Data.Time hiding (Day, parseTime)
 
 -- | "Target" datatype for the DateTime parser, i.e, the parser should produce elements of this type.
 data DateTime = DateTime { date :: Date
@@ -15,8 +18,14 @@ data Date = Date { year  :: Year
     deriving (Eq, Ord)
 
 newtype Year  = Year  { runYear  :: Int } deriving (Eq, Ord)
+instance Show Year where
+  show Year{..} = show runYear
 newtype Month = Month { runMonth :: Int } deriving (Eq, Ord)
+instance Show Month where
+  show Month{..} = show runMonth
 newtype Day   = Day   { runDay   :: Int } deriving (Eq, Ord)
+instance Show Day where
+  show Day{..} = show runDay
 
 data Time = Time { hour   :: Hour
                  , minute :: Minute
@@ -24,25 +33,68 @@ data Time = Time { hour   :: Hour
     deriving (Eq, Ord)
 
 newtype Hour   = Hour   { runHour   :: Int } deriving (Eq, Ord)
+instance Show Hour where
+  show Hour{..} = show runHour
 newtype Minute = Minute { runMinute :: Int } deriving (Eq, Ord)
+instance Show Minute where
+  show Minute{..} = show runMinute
 newtype Second = Second { runSecond :: Int } deriving (Eq, Ord)
+instance Show Second where
+  show Second{..} = show runSecond
 
 
 -- Exercise 1
 parseDateTime :: Parser Char DateTime
-parseDateTime = undefined
+parseDateTime = DateTime <$> parseDate
+                         <*  symbol 'T'
+                         <*> timeParser
+                         <*> parseUTC
+
+parseNatOfNLength :: Int -> Parser Char Int
+parseNatOfNLength n = foldl (\a b -> a * 10 + b) 0 <$> sequence (replicate n newdigit)
+
+parseNatOfLengthTwo :: Parser Char Int
+parseNatOfLengthTwo = parseNatOfNLength 2
+
+parseDate :: Parser Char Date
+parseDate =
+  Date <$> (Year <$> parseNatOfNLength 4)
+    <*> (Month <$> parseNatOfLengthTwo)
+    <*> (Day <$> parseNatOfLengthTwo)
+
+timeParser :: Parser Char Time
+timeParser = Time <$> (Hour <$> parseNatOfLengthTwo)
+                <*> (Minute <$> parseNatOfLengthTwo)
+                <*> (Second <$> parseNatOfLengthTwo)
+
+parseUTC :: Parser Char Bool
+parseUTC = option (True <$ symbol 'Z') False
 
 -- Exercise 2
-run :: Parser a b -> [a] -> Maybe b
-run = undefined
+-- a lookup variant that searches according to the second tuple element 
+lookup' :: Eq b => b -> [(a, b)] -> Maybe a
+lookup' x ys = lookup x $ map swap ys
+
+-- run :: Parser a b -> [a] -> Maybe b
+run :: Eq a => Parser a b -> [a] -> Maybe b
+run p input = lookup' [] $ parse p input
 
 -- Exercise 3
+-- Record syntax documentation: https://devtut.github.io/haskell/record-syntax.html
 printDateTime :: DateTime -> String
-printDateTime = undefined
+printDateTime (DateTime d t u) = printDate d ++ printTime t ++ (if u then "Z" else "")
+  where
+    printDate :: Date -> String
+    printDate Date{..} = show year ++ show month ++ show day
+    printTime :: Time -> String
+    printTime Time{..} = show hour ++ show minute ++ show second
 
--- Exercise 4
-parsePrint s = fmap printDateTime $ run parseDateTime s
+-- -- Exercise 4
+parsePrint :: String -> Maybe String
+parsePrint s = printDateTime <$> run parseDateTime s
 
+-- https://williamyaoh.com/posts/2019-09-16-time-cheatsheet.html
+-- https://hackage.haskell.org/package/number-length
 -- Exercise 5
 checkDateTime :: DateTime -> Bool
 checkDateTime = undefined
